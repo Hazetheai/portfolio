b0951f759ca991d865318dfc4895d736198899b8
Jake <jacobstefan92@gmail.com>
2019-05-10 22:56:23 -0400

    added scroll to projects

--- js/scroll.js
+++ js/scroll.js
@@ -1,29 +1,12 @@
 const projectNav = document.querySelector("#projectNav");
 const projects = document.querySelector("#section-projects");
 
-function scrollTo(to, duration) {
-  //if to == 0 return
-  if (document.body.scrollTop == to) return;
-  //diff is the difference between
-  let diff = to - document.body.scrollTop;
-  const scrollStep = Math.PI / (duration / 10);
-  let count = 0,
-    currPos;
-  start = window.pageXOffset;
-  scrollInterval = setInterval(function() {
-    if (document.body.scrollTop != to) {
-      count = count + 1;
-      currPos = start + diff * (0.5 - 0.5 * Math.cos(count * scrollStep));
-      document.body.scrollTop = currPos;
-    } else {
-      clearInterval(scrollInterval);
-    }
-  }, 10);
-}
+projectNav.addEventListener("click", () =>
+  projects.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "nearest"
+  })
+);
 
-function test(elID) {
-  var dest = document.getElementById(elID);
-  scrollTo(dest.offsetTop, 500);
-}
-
-window.addEventListener("scroll", () => console.log(projects.scrollTop));
+// window.addEventListener("scroll", () => console.log(pageYOffset));
